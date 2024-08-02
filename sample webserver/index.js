a689fb0e6f313823b0346e021202ca67c4c555b3
vurajyothisri <vurajyothisri@gmail.com>
2024-08-02 21:09:17 +0530

    simple webserver

---
+++ sample webserver/index.js
@@ -0,0 +1,8 @@
+const http=require('http')
+ const server=http.createServer((req,res)=>{
+    res.end("Hello! from the server")
+ })
+
+ server.listen(8000,'127.0.0.1',()=>{
+    console.log('Listening to requesting')
+ })
